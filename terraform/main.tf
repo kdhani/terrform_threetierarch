39ab634125c8b779a5c1ee11a8f6396ce74e53af
kdhani <37719348+kdhani@users.noreply.github.com>
2024-03-25 12:03:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,11 @@ provider "aws" {
   profile = "default"
   region  = var.region
 }
+
+data "aws_secretsmanager_secret" "database_credentials" {
+name="sql_credentials_1" #name of secret in aws secrets manager
+}
+resource "aws_db_instance" "mysql" {
+username=data.aws_secretsmanager_secret.database_credentials.secret["username"]
+password=data.aws_secretsmanager_secret.database_credentials.secret["password"]
+}
